06542be62add151d8eab2b6e88ea28b5bf865d2a
gk07 <gk_520@hotmail.com>
2020-08-21 16:16:10 +0800

    Update persist.java

--- codeWar_Java/Persist/Persist.java
+++ codeWar_Java/Persist/Persist.java
@@ -1,13 +1,18 @@
 public class Persist {
     public static void main(String[] args){
-        int i = 0;
-        int number = 958;
-
+        int i = 0, number = 958, res;
         do {
-            int res = converter(number);
+            i++;
+            res = converter(number);
             System.out.println(res);
-        } while (number != res);
+            if ( res == number) {
+                break;
+            } else {
+                number = res;
+            }
+        } while (true);
 
+        System.out.println(i);
     }
 
     static int converter(int arg) {
